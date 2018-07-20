8bfe87676b3907e484a31c490fd9456a99e5da25
shprasad <sharath.prasad@amadeus.com>
2018-07-20 10:04:27 +0530

    data commited for the form

--- src/app/modules/travelbuzz/components/form/form.component.ts
+++ src/app/modules/travelbuzz/components/form/form.component.ts
@@ -12,4 +12,11 @@ export class FormComponent implements OnInit {
   ngOnInit() {
   }
 
+  field1 = {'name':'Source','type':'text','iconSide':'left-side','icon':'fa fa-search'};
+  field2 = {'name':'Destination','type':'text','iconSide':'left-side','icon':'fa fa-search'};
+  field3 = {'name':'Email','type':'text','iconSide':'left-side','icon':'fas fa-envelope'};
+  field4 = {'name':'Date','type':'select','iconSide':'left-side','icon':'fal fa-calendar-alt'};
+  field5 = {'name':'Pax','type':'select','iconSide':'left-side','icon':'fa fa-users'};
+  field6 = {'name':'Mode of Travel','type':'select','iconSide':'left-side'};
+
 }
